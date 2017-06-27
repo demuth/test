ecc5fe976a9487ae9c78b01d93868094f940d64a
Derik-B <d.brockopp@vcsu.edu>
2017-06-27 15:34:47 -0500

    fixed file
    File will now successfully read information from the 'inventory' table on the 'DUNE' database

--- ReadFromApplePi.py
+++ ReadFromApplePi.py
@@ -1,16 +1,17 @@
 import mysql.connector
 import datetime
 
-cnx = mysql.connector.connect(user="root", password="rhoades303", host="10.0.1.6", database="DUNE")
+cnx = mysql.connector.connect(user="root", password="Rhoades104D", host="10.0.1.6", database="DUNE")
 
 cursor = cnx.cursor()
 
-query = ("SELECT * FROM 'DUNE' 'inventory'")
+query = ("SELECT device_name, device_type, operator_id, created FROM inventory")
 
 cursor.execute(query)
 
 for (device_name, device_type, operator_id, created) in cursor:
-    print("{}, of type {} was checked in by{}, at {}.")
+    print("{}, of type {} was checked in by {}, at {}.".format(
+        device_name, device_type, operator_id, created))
     
 cursor.close()
 cnx.close()
