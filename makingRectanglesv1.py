9af6196d38cc4df0ff75b2a6df008a5198bb010f
Derik-B <d.brockopp@vcsu.edu>
2017-07-11 14:38:37 -0500

    fixing a comment

--- makingRectanglesv1.py
+++ makingRectanglesv1.py
@@ -1,7 +1,7 @@
 from Tkinter import *
 
 i=0
-k=14 #this is where we would read in the values
+k=14 #this is where we would read in the number of rectangles needed
 leftx=50; lefty=25; rightx=300; righty=75#coordinates for the rectangle to be used in the w.create
 #left refers to the top left corner of the rectangle, right refers to the bottom right corner
 
