d01601dc06f6da690d6988942b47ce8f26d8a274
Derik-B <d.brockopp@vcsu.edu>
2017-07-11 15:01:34 -0500

    added comment

--- findScreenSize.py
+++ findScreenSize.py
@@ -6,3 +6,4 @@ screen_width=root.winfo_screenwidth()
 screen_height=root.winfo_screenheight()
 
 print screen_width, screen_height
+#the variables screen_width and screen_height can be used as the respective parameters when creating a canvas
