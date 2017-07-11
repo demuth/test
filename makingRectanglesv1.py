83bac6b57870631618d8b4d34cdfa44fdceb3136
Derik-B <d.brockopp@vcsu.edu>
2017-07-11 14:35:09 -0500

    small while loop to add rectangles to a canvas

---
+++ makingRectanglesv1.py
@@ -0,0 +1,19 @@
+from Tkinter import *
+
+i=0
+k=14 #this is where we would read in the values
+leftx=50; lefty=25; rightx=300; righty=75#coordinates for the rectangle to be used in the w.create
+#left refers to the top left corner of the rectangle, right refers to the bottom right corner
+
+master=Tk();
+
+w=Canvas(master, width=600, height=750)
+w.pack()
+
+
+while (i<k):
+    w.create_rectangle(leftx, lefty, rightx, righty)
+    lefty=lefty+50; righty=righty+50
+    i=i+1
+
+master.mainloop()
